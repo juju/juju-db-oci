3f8364bbc02d57bde6b4960c51b1ee20db7044cc
Harry Pidcock <harry.pidcock@canonical.com>
2022-11-17 16:45:24 +1000

    Setup workflows for publishing oci images.

--- make_functions.sh
+++ make_functions.sh
@@ -37,6 +37,7 @@ build_image() {
     echo "You must supply the docker buildx output to use for image ${image}"
     exit 1
   fi
+  is_local=${3-0}
 
   dockerfile=Dockerfile
   reg_paths=$(yq -o=c ".images.[\"${image}\"].registry_paths" < images.yaml)
@@ -47,7 +48,29 @@ build_image() {
   cmd_build_args=$(_build_args_builder "$build_args")
   cmd_tags=$(_tag_argument_builder "$reg_paths" "$tags")
 
+  if [[ "${is_local}" -eq 1 ]]; then
+    cmd_platforms=""
+  else
+    cmd_platforms="--platform \"${platforms}\""
+  fi
+
   echo "Building ${image} for build args \"$build_args\" and platforms \"$platforms\""
-  docker buildx build --platform "$platforms" ${cmd_build_args} \
+  echo ${cmd_platforms}
+  docker buildx build ${cmd_platforms} ${cmd_build_args} \
     -f "$dockerfile" . ${cmd_tags} -o "$output" --progress=auto
+}
+
+import_microk8s() {
+  image=${1-""}
+  if [ -z "$image" ]; then
+    echo "You must supply the image to build in images.yaml"
+    exit 1
+  fi
+  reg_paths=$(yq -o=c ".images.[\"${image}\"].registry_paths" < images.yaml)
+  tags=$(yq -o=c ".images.[\"${image}\"].tags" < images.yaml)
+  for reg_path in ${reg_paths//,/ }; do
+    for tag in ${tags//,/ }; do
+      docker save "${reg_path}:${tag}" | microk8s.ctr --namespace k8s.io image import -
+    done
+  done
 }
